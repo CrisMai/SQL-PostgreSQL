e128657b61ecd6878c55d77515b59d46fefa7c97
unknown <crismai.dev@gmail.com>
2023-08-31 16:29:44 -0300

    Quueries aninhadas

---
+++ PostgreSQL_views, funcoes/queries-aninhadas.sql
@@ -0,0 +1,7 @@
+Queries aninhadas, também conhecidas como subconsultas, 
+são consultas SQL que são inseridas dentro de outras consultas. 
+
+ Elas permitem que você execute uma consulta dentro de outra consulta, 
+ permitindo operações mais complexas e precisas. As subconsultas podem 
+ ser usadas em várias partes de uma consulta maior, como na cláusula WHERE, 
+ cláusula FROM ou até mesmo na cláusula SELECT.
