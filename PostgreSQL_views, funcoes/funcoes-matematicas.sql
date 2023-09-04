74114ac8fad1ae79f39c02c501e4f11d0635980c
unknown <crismai.dev@gmail.com>
2023-09-04 16:58:04 -0300

    Funções matemáticas

---
+++ PostgreSQL_views, funcoes/funcoes-matematicas.sql
@@ -0,0 +1,13 @@
+Além dos operaadores simples + , - , * e /, existem
+algumas funções matemáticas que podem ser utilizadas.
+
+Exemplos de algumas funções matemáticas:
+
+- retornar a raiz ( cbrt(dp) )
+- arredondar o valor para cima ( ceil(dp or numeric) ) ou 
+para baixo ( ceiling(dp or numeric) )
+- converter de graus para radianos ( degrees(dp)) 
+- retornar o quociente inteiro ( div(y numeric, x numeric) ) 
+- realizar operações exponenciais ( exp(dp or numeric) )
+- conversão de bases ( floor(dp or numeric) )
+- logaritmo, obter o pi ( pi() ), entre outras.
