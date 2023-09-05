a2b666d93415fd770f0ec7068c23dae05616cdc5
unknown <crismai.dev@gmail.com>
2023-09-05 17:55:34 -0300

    Entendendo o conceito de view

---
+++ PostgreSQL_views, funcoes/views.sql
@@ -0,0 +1,22 @@
+Uma view é uma consulta SQL armazenada que fornece uma representação 
+personalizada e virtual dos dados em uma ou mais tabelas de um banco 
+de dados. Elas são usadas para simplificar consultas, melhorar a 
+segurança e fornecer uma maneira conveniente de trabalhar com dados 
+em um banco de dados.
+
+
+Também podemos entender o conceito de view o ato de nomear consultas, 
+utilizando tabelas virtuais a partir de um nome atribuído a ela.
+Esse conceito é muito utilizado para, por exemplo, fornecer apenas 
+uma parte do banco de dados para um fornecedor externo, ou um cliente, 
+porque podemos entregar o acesso apenas dessas views do sistema.
+
+
+Dessa forma temos mais segurança, dado que a view é apenas uma tabela 
+virtual, não sendo possível alterar ou manipular os dados, apenas 
+consultá-los. Assim temos uma segurança maior para fornecermos os dados.
+
+
+Portanto, o que queremos, utilizando as views, é nomear consultas, seja 
+para termos melhor legibilidade na construção e utilização de relatórios,
+ ou para fornecer acesso ao banco de dados para terceiros.
