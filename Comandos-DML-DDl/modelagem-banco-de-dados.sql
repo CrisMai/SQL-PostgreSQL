080b22ad34e38e8027dab827f72891c3985031ae
unknown <crismai.dev@gmail.com>
2023-09-08 14:55:09 -0300

    Modelagem de banco de dados

---
+++ Comandos-DML-DDl/modelagem-banco-de-dados.sql
@@ -0,0 +1,23 @@
+
+Modelagem de Banco de dados
+
+A modelagem de banco de dados é o processo de criação de uma 
+representação estruturada e organizada de como os dados serão 
+armazenados, organizados e relacionados em um sistema de banco 
+de dados. É uma técnica que define as tabelas, campos, 
+relacionamentos e restrições que compõem o esquema de um banco 
+de dados, permitindo a eficiente organização e recuperação de 
+informações.
+
+
+DML e DDL
+
+DML e DDL são duas categorias diferentes de comandos usados em 
+sistemas de gerenciamento de bancos de dados (DBMS).
+
+A DML se concentra na manipulação dos dados armazenados em um banco 
+de dados (consultar, inserir, atualizar e excluir registros).
+
+A DDL lida com a definição da estrutura do banco de dados em 
+si (criar, modificar e excluir tabelas, índices e outros objetos 
+relacionados ao esquema do banco de dados). 
