7f076afc02ae420334b6cdee6b65d910f89b24f2
unknown <crismai.dev@gmail.com>
2023-10-01 18:19:06 -0300

    Schemas

---
+++ Comandos-DML-DDl/Schemas.sql
@@ -0,0 +1,17 @@
+
+Schemas
+
+Em bancos de dados, um "schema" (esquema) é uma estrutura lógica 
+que define como os dados são organizados e como as tabelas, visões, 
+procedimentos armazenados e outros objetos relacionados ao banco de 
+dados estão agrupados.
+
+O schema fornece um contexto ou namespace para esses objetos, ajudando 
+a evitar conflitos de nome e organizando de forma mais eficaz as informações 
+no banco de dados. 
+
+Geralmente, um banco de dados pode conter vários schemas, cada um representando 
+um conjunto de objetos relacionados ou pertencentes a uma parte específica do 
+aplicativo ou sistema.
+
+O propósito de um schema é organizar (e separar) de forma lógica as tabelas.
