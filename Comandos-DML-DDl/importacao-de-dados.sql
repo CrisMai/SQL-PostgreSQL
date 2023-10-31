f55d1ae6903aa998e4c8569ee655544d4033de1a
unknown <crismai.dev@gmail.com>
2023-10-31 18:54:06 -0300

    Importação de dados

---
+++ Comandos-DML-DDl/importacao-de-dados.sql
@@ -0,0 +1,11 @@
+
+Importação de dados
+
+A importação de dados é o processo de transferir informações de uma fonte externa, 
+como um arquivo CSV, Excel, outra base de dados ou qualquer outra origem, para um banco 
+de dados. Isso é feito para armazenar, analisar ou trabalhar com os dados de forma mais 
+eficaz no sistema de gerenciamento de banco de dados. 
+
+Geralmente, os sistemas de gerenciamento de banco de dados oferecem ferramentas e utilitários 
+para realizar a importação de dados de maneira eficiente e consistente, garantindo que os dados 
+sejam formatados corretamente e estejam prontos para uso.
