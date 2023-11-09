6d7fc2ad8ee64c3d9035718302cd619d152cd150
unknown <crismai.dev@gmail.com>
2023-11-09 17:59:29 -0300

    Tipo de dado Enum

---
+++ Comandos-DML-DDl/enum.sql
@@ -0,0 +1,13 @@
+
+Tipo de dado - ENUM
+
+O tipo de dado ENUM (Enumeração) em bancos de dados é usado para representar 
+um conjunto finito de valores possíveis. Ele é útil quando você deseja restringir 
+os valores que uma coluna pode conter a um conjunto específico de opções predefinidas. 
+O ENUM permite que você defina um domínio de valores possíveis para uma coluna, tornando-a 
+mais segura e legível. 
+Por exemplo, você pode usar ENUM para criar uma coluna "Status" que aceita apenas valores 
+como "Ativo", "Inativo" ou "Pendente". Isso evita a entrada de valores inválidos.
+
+Ao utilizar o tipo ENUM, podemos garantir que os valores de uma coluna estejam dentro do 
+conjunto permitido de opções, proporcionando maior controle e segurança aos dados.
